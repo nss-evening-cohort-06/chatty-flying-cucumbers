bd8d71092e8ae3742d9e3b5cc04ccd95122732a2
Juno Lee <lejuno80@gmail.com>
2017-09-25 19:12:13 -0500

    clear button disable issue fix

--- dist/app.js
+++ dist/app.js
@@ -87,16 +87,25 @@ module.exports = {initializer, getMessages, createNewMessage,};
 "use strict";
 
 const domString = require('./dom');
+let outputDiv = document.getElementById("mainContainer");
+
 
 const deleteButton = (e) => {
 	if(e.target.className ==='deleteButton'){
 		e.target.parentElement.remove();
-	}
+		console.log("inside delete", document.getElementsByClassName('deleteButton'));
+		if(document.getElementsByClassName('deleteButton').length === 0) {
+  			document.getElementById('clearBtn').disabled = true;
+
+		}
+	} 
 };
 
 document.body.addEventListener("click", function (e){
 	deleteButton(e);
 });
+//
+
 
 module.exports = deleteButton;
 },{"./dom":5}],5:[function(require,module,exports){
